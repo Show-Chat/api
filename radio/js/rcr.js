c439d788cd81dbeadd4f486a71f26a2dc964a330
ShowChat <34014529+Show-Chat@users.noreply.github.com>
2020-06-27 16:54:57 +0200

    Update rcr.js

--- radio/js/rcr.js
+++ radio/js/rcr.js
@@ -14,7 +14,7 @@
 			enable_cors: false,
 			cors: "https://rcr-cors.herokuapp.com",
 			artwork: true,
-            logo: "images/no-cover-large.png",
+            logo: "https://api.showchat.tk/images/no-cover-large.png",
 			vertical_layout: false,
             bg: "grey",
             accent: "deeporange",
